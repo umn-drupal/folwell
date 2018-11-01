097ffa78a2f9fdc600120e894d3979f0a23f1bc0
Jim Hart <HART0554@UMN.EDU>
2018-11-01 14:32:07 -0500

    add unit identity settings

--- theme-settings.php
+++ theme-settings.php
@@ -16,6 +16,8 @@ function folwell_form_system_theme_settings_alter(&$form, FormStateInterface &$f
     return;
   }
 
+  $site_config = \Drupal::config('system.site');
+  $site_name = !empty($site_config->get('name')) ? $site_config->get('name') : '';
   unset($form['logo']);
   $form['theme_settings']['#open'] = FALSE;
   $form['favicon']['default_favicon']['#default_value'] = theme_get_setting('favicon.use_default');
@@ -85,6 +87,55 @@ function folwell_form_system_theme_settings_alter(&$form, FormStateInterface &$f
     '#title' => t('Mobile search box setting'),
     '#description' => t('Configure how search appears on small screens - expandable with a dropdown button, or always visible'),
   ];
+  $form['site_identity'] = [
+    '#type' => 'details',
+    '#title' => t('Site/Unit Identity'),
+    '#open' => TRUE,
+  ];
+  $form['site_identity']['first_line'] = [
+    '#type' => 'textfield',
+    '#states' => [
+      'disabled' => [
+        ':input[name="parent_unit"]' => [
+          'empty' => FALSE
+        ]
+      ]
+    ],
+    '#title' => t('First line'),
+    '#description' => t('Appears above site name. Useful for units with long names. You can only include one of "first line" or "parent unit" fields.'),
+    '#default_value' => theme_get_setting('first_line')
+  ];
+  $form['site_identity']['site_name'] = [
+    '#type' => 'textfield',
+    '#title' => t('Site name'),
+    '#default_value' => !empty(theme_get_setting('site_name')) ? theme_get_setting('site_name') : $site_name
+  ];
+  $form['site_identity']['parent_unit'] = [
+    '#type' => 'textfield',
+    '#states' => [
+      'disabled' => [
+        ':input[name="first_line"]' => [
+          'empty' => FALSE
+        ]
+      ]
+    ],
+    '#title' => t('Parent unit'),
+    '#description' => t('Appears above site name. Use to designate a College or parent administrative unit. You can only include one of "parent unit" or "first line" fields.'),
+    '#default_value' => theme_get_setting('parent_unit')
+  ];
+  $form['site_identity']['parent_url'] = [
+    '#type' => 'url',
+    '#title' => t('Parent unit url'),
+    '#description' => t('Provide a url for the parent unit; include http/https.'),
+    '#default_value' => theme_get_setting('parent_url'),
+    '#states' => [
+      'visible' => [
+        ':input[name="parent_unit"]' => [
+          'empty' => FALSE
+        ]
+      ]
+    ]
+  ];
   $form['footer_options'] = [
     '#type' => 'details',
     '#title' => t('Footer options'),
@@ -93,7 +144,7 @@ function folwell_form_system_theme_settings_alter(&$form, FormStateInterface &$f
   $form['footer_options']['privacy_link'] = [
     '#type' => 'textfield',
     '#default_value' => theme_get_setting('privacy_link'),
-    '#title' => 'Privacy link',
+    '#title' => t('Privacy link'),
     '#description' => t('If you are using an alternative privacy statement, replace http://privacy.umn.edu with the appropriate url (include "http://" if on another site, or an absolute path - begins with "/" - if using a page on this website).'),
   ];
   $form['footer_options']['footer_date'] = [
