3843d2ac1d4ec08573341efdcbc8ee13995d0538
Jim Hart <HART0554@UMN.EDU>
2018-11-02 14:13:14 -0500

    add setting for controlling page width

--- theme-settings.php
+++ theme-settings.php
@@ -20,7 +20,22 @@ function folwell_form_system_theme_settings_alter(&$form, FormStateInterface &$f
   $site_name = !empty($site_config->get('name')) ? $site_config->get('name') : '';
   unset($form['logo']);
   $form['theme_settings']['#open'] = FALSE;
+  $form['favicon']['#open'] = FALSE;
   $form['favicon']['default_favicon']['#default_value'] = theme_get_setting('favicon.use_default');
+  $form['page_options'] = [
+    '#type' => 'details',
+    '#title' => t('Page options'),
+    '#open' => TRUE,
+  ];
+  $form['page_options']['page_width'] = [
+    '#type' => 'radios',
+    '#title' => t('Page width'),
+    '#default_value' => theme_get_setting('page_width') ? theme_get_setting('page_width') : 'fixed-width',
+    '#options' => [
+      'fixed-width' => 'Fixed width (1200px)',
+      'full-width' => 'Full width (100%)'
+    ]
+  ];
   $form['header_options'] = [
     '#type' => 'details',
     '#title' => t('Header options'),
