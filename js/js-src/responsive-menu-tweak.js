f15cfddb16744f6d228b955d9f895dc280fd51e7
Jim Hart <HART0554@UMN.EDU>
2019-06-03 11:40:24 -0500

    add alt/label for mobile menu (fixes #16), prettier style tweaks

--- js/js-src/responsive-menu-tweak.js
+++ js/js-src/responsive-menu-tweak.js
@@ -1,15 +1,15 @@
 (function(Drupal, $) {
-
   Drupal.behaviors.responsiveMenuTweak = {
     attach: function() {
-      var navbar = $('.mm-navbar'),
-          wordmark = '<img id="mobile-nav-m" src="/themes/custom/folwell/images/branding/wordmarks/M-wordmark.png">',
-          closeButton = '<a class="mm-btn" id="close-nav" href="#mm-0" aria-owns="mm-0"></a>';
-      if (!navbar.hasClass('wordmarked')) {
+      var navbar = $(".mm-navbar"),
+        wordmark =
+          '<img id="mobile-nav-m" src="/themes/custom/folwell/images/branding/wordmarks/M-wordmark.png" alt="UMN block M wordmark">',
+        closeButton =
+          '<a class="mm-btn" id="close-nav" href="#mm-0" aria-owns="mm-0" aria-label="close menu"></a>';
+      if (!navbar.hasClass("wordmarked")) {
         navbar.append(wordmark).append(closeButton);
-        navbar.addClass('wordmarked');
+        navbar.addClass("wordmarked");
       }
     }
   };
-
 })(Drupal, jQuery);
